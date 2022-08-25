43cc2010f4ebb13bd78f1fe86d2968f4619a8743
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-08-25 23:19:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot=commands.Bot(command_prefix='!')
-
+bot = commands.Bot(command_prefix='/',intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
